0b46fec343166d49e8c6240934185b49c57b53d2
BalkrishanSingh <Balkrishan62005@Gmail.com>
2021-12-14 16:26:03 +0530

    Added Finder of Volume Of Sphere.

---
+++ VolumeSphere.py
@@ -0,0 +1,5 @@
+# Volume of a Sphere of radius r is equal to 4/3 * Pi * r**3
+PI = 3.14159
+radius =  int(input('Find The Volume of Sphere Of Radius : '))
+volume = 4/3 * PI * radius**3
+print(f'The Volume of Sphere Of Radius {radius} is {round(volume,2)}')
