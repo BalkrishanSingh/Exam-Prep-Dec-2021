82806062b53a78f265564ea6131bf5550346f948
BalkrishanSingh <Balkrishan62005@Gmail.com>
2021-12-15 19:03:45 +0530

    Added Alternate Solution for Fibonacci

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,6 +1,10 @@
 #Fibonacci Series is a series where each number is the sum of the preceding two numbers
 n = int(input('Get Fibanacci upto number : '))
-numbers = [i for i in range(n+1)]
+numbers = [0,1]
+previous = 0
+current = 1
 for i in range(1,n):
-    numbers[i+1] = numbers[i-1] + numbers[i]
+    next_num = previous + current
+    numbers.append(next_num)
+    previous,current = current,next_num
 print(f'Fibonacci Series upto {n} is :',*numbers)
