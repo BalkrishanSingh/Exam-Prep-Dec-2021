e9d339ee9fe4a6057ad0104bae9422a8fad83daf
BalkrishanSingh <Balkrishan62005@Gmail.com>
2021-12-15 19:29:07 +0530

    Changed for loop

--- CountElements.py
+++ CountElements.py
@@ -1,6 +1,6 @@
 #Counting the number Of elements in a list
 items = eval(input("Enter The List : "))
 count = 0
-for _ in range(len(items)):
+for _ in items:
     count += 1
 print("No of Elements in the list",count)
