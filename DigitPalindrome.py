f740e02192fe37c4c0616a6cc710eed0d639c690
BalkrishanSingh <Balkrishan62005@Gmail.com>
2021-12-15 20:40:57 +0530

    Alternate Method without using string slicing

--- DigitPalindrome.py
+++ DigitPalindrome.py
@@ -1,8 +1,12 @@
 #A Number which is equal to its reverse is called a Palindromic number.
-number = input('Enter Number : ')
-reversed_digit = int(number[::-1])
-number = int(number)
-if number == reversed_digit:
+number = int(input('Enter Number : '))
+temp = number
+reversed_num = 0
+while temp !=0:
+    digit = temp % 10
+    reversed_num = reversed_num * 10 + digit
+    temp //= 10
+if number == reversed_num:
     print('The number is a palindrome')
 else:
     print('Number is not a palindrome')
